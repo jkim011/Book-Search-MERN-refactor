2a41693f1d2e23cd28ef81a235888dff91d67f31
Jared Kim <jaredkim01@yahoo.com>
2023-02-20 12:55:43 -0600

    finished queries.js and mutations.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,18 @@
 import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
-  
-`
+  me {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      description
+      title
+      image 
+      link
+    }
+  }
+`;
